2467316121787c6cf3e38699267ec60213b2777e
Joe771010 <cvbridgechiao@gmail.com>
2018-12-05 13:23:52 +0800

    fragile player 2 build commit

---
+++ FragilePlayer2/precache-manifest.d34183151719e2061f5b78b509bf8747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4069369ebed97865a9c2",
+    "url": "/static/js/main.4069369e.chunk.js"
+  },
+  {
+    "revision": "4ca3b6db0e404102b182",
+    "url": "/static/js/1.4ca3b6db.chunk.js"
+  },
+  {
+    "revision": "4069369ebed97865a9c2",
+    "url": "/static/css/main.2aab0322.chunk.css"
+  },
+  {
+    "revision": "3c510a0d25d5771a05288025858d86e7",
+    "url": "/index.html"
+  }
+];
